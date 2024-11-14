dfbf2071fdb2f2058bad24708081291018f17fe6
Agnesamani <150405608+Agnesamani@users.noreply.github.com>
2024-11-14 20:06:50 +0100

    client commit

--- Client.cs
+++ Client.cs
@@ -1,3 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.Net.Sockets;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        string serverIp = "10.180.35.232"; 
+        int port = 8082;
