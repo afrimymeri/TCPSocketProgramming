7cd140b6d0e0176cab96670428e4bdd00ad11739
Agnesamani <150405608+Agnesamani@users.noreply.github.com>
2024-11-14 20:09:10 +0100

    commit

--- Client.cs
+++ Client.cs
@@ -8,3 +8,12 @@ class Client
     {
         string serverIp = "10.180.35.232"; 
         int port = 8082;
+        try
+        {
+            using (TcpClient client = new TcpClient(serverIp, port))
+            using (NetworkStream stream = client.GetStream())
+            using (StreamWriter writer = new StreamWriter(stream) { AutoFlush = true })
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                Console.Write("Enter your name: ");
+                string clientName = Console.ReadLine();
